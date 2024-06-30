b50c6d3679d23c22299c083e88974c4b51bbdb3d
Bhrigu verma <bhriguverma.bt23cse@pec.edu.in>
2024-06-30 20:20:21 +0530

    new chnAnges made!

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,11 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.tsx";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
         <BrowserRouter>
