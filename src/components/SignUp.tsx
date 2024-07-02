d784cb5521e686dab81f9d3a3fae0e4c5cf2c0a0
Bhrigu verma <146697095+bhrigu-verma@users.noreply.github.com>
2024-07-02 10:43:55 +0530

    Update SignUp.tsx

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import BackgroundImage from './BackgroundImage';
-import { SignUp } from '@clerk/clerk-react';
+// import React from 'react';
+// import BackgroundImage from './BackgroundImage';
+// import { SignUp } from '@clerk/clerk-react';
 
-const SignUpPage = () => {
-  return (<>
-    <BackgroundImage />
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className='content absolute inset-0 items-center z-10 justify-center mx-[459px] mt-[90px] bg-gray-900 bg-opacity-50'>
+// const SignUpPage = () => {
+//   return (<>
+//     <BackgroundImage />
+//     <div className="flex flex-col items-center justify-center h-screen">
+//       <div className='content absolute inset-0 items-center z-10 justify-center mx-[459px] mt-[90px] bg-gray-900 bg-opacity-50'>
       
-      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in"   redirectUrl="/home"  /></div>
-    </div></>
-  );
-};
+//       <SignUp path="/sign-up" routing="path" signInUrl="/sign-in"   redirectUrl="/home"  /></div>
+//     </div></>
+//   );
+// };
 
-export default SignUpPage;
+// export default SignUpPage;
